1ee59e2b45ea92f62b829cf29017137bf134de2b
Giorgi Kratsashvili <gkrat17@freeuni.edu.ge>
2021-06-26 23:58:01 +0400

    add delete task by id service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,7 +12,7 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id == id);
+    return this.tasks.find((task) => task.id === id);
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -28,5 +28,9 @@ export class TasksService {
     this.tasks.push(task);
 
     return task;
+  }
+
+  deleteTaskById(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
